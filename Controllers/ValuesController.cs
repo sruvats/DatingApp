3aafa01427d91c8c3262f5b9b0f3c82e8dd929e2
Sruthi Vasudevan <sruthivasudevan.edu@gmail.com>
2020-07-04 16:54:18 -0500

    new commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public ValuesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+           // return new string[] { "value1", "value2" };
+           var values= await _dataContext.Values.ToListAsync();
+           return Ok(values);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+           var value= await _dataContext.Values.FirstOrDefaultAsync(x=>x.Id==id);
+           return Ok(value);
+        }
+
+        [HttpPost]
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+        [HttpPut]
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete]
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
